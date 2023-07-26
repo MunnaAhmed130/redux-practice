07645c82c02ab2c3d96f102646812ae24c60071c
Munna Ahmed <munnaahmed2025@gmail.com>
2023-07-26 20:39:13 +0600

    middleware and redux dev tool

--- react-redux-demo/src/components/IceCreamContainer.jsx
+++ react-redux-demo/src/components/IceCreamContainer.jsx
@@ -5,7 +5,7 @@ const IceCreamContainer = (props) => {
   return (
     <div>
       <h2>Number of ice Cream - {props.numOfIceCreams}</h2>
-      <button onClick={props.buyIceCream}>Buy Cake</button>
+      <button onClick={props.buyIceCream}>Buy IceCream</button>
     </div>
   );
 };
