810ed57d9eb5a9d673d7ba8f9bcd73f9c463264b
Munna Ahmed <munnaahmed2025@gmail.com>
2023-07-27 23:49:43 +0600

    multiple reducer

--- react-redux-demo/src/App.jsx
+++ react-redux-demo/src/App.jsx
@@ -1,10 +1,10 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
-import IceCreamContainer from "./components/IceCreamContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
-import ItemContainer from "./components/ItemContainer";
+// import CakeContainer from "./components/CakeContainer";
+// import HooksCakeContainer from "./components/HooksCakeContainer";
+// import IceCreamContainer from "./components/IceCreamContainer";
+// import NewCakeContainer from "./components/NewCakeContainer";
+// import ItemContainer from "./components/ItemContainer";
 import UserContainer from "./components/UserContainer";
 
 function App() {
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+  require("./features/icecream/iceCreamSlice").icecreamActions;
 
 console.log("Initial state", store.getState());
 
@@ -11,5 +13,8 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe();
