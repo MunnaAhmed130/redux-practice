497281377b3f8394f0a96f532084465fc98ca3f9
Munna Ahmed <munnaahmed2025@gmail.com>
2023-07-29 08:42:42 +0600

    react-rtk-demo app

---
+++ react-rtk-demo/src/app/store.js
@@ -0,0 +1,15 @@
+import configureStore from "@reduxjs/toolkit";
+import { cakeReducer, icecreamReducer, userReducer } from "../features";
+
+// const logger = reduxLogger.createLogger();
+
+const store = configureStore({
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+    user: userReducer,
+  },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
---
+++ react-rtk-demo/src/features/index.js
@@ -0,0 +1,9 @@
+import cakeSlice from "./cake/cakeSlice";
+import iceCreamSlice from "./icecream/iceCreamSlice";
+import userSlice from "./user/userSlice";
+
+export {
+  cakeSlice as cakeReducer,
+  iceCreamSlice as icecreamReducer,
+  userSlice as userReducer,
+};
