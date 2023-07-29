3228db81fc07be1027b5fa8c94811c7dc1b1f4ee
Munna Ahmed <munnaahmed2025@gmail.com>
2023-07-29 08:14:44 +0600

    async thunk with redux toolkit

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,20 +1,24 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const { fetchUsers } = require("./features/user/userSlice");
 const icecreamActions =
   require("./features/icecream/iceCreamSlice").icecreamActions;
 
 console.log("Initial state", store.getState());
 
 const unsubscribe = store.subscribe(() => {
-  // console.log("Updated state", store.getState());
+  console.log("Updated state", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
+store.dispatch(fetchUsers());
 
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
+
+// unsubscribe();
